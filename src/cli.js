098be923654b9ad4df8c7e067aef94539dc2c65c
Ilshat Fattakhov <fatah.guitar@gmail.com>
2022-03-22 00:53:36 +0700

    fix errors

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-export var userName = readlineSync.question('May i have your name?');
-console.log('Hello ' + userName  + '!');
+const nameUser = (text) => {
+  text = readlineSync.question('May I have your name? ');
+  const name = (text === '') ? 'new player' : text;
+  console.log(`Hello, ${name}!`);
+};
+export default nameUser;
