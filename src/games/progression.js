52222738dc039acc41f9bb44773e7a14e8641290
Ilshat Fattakhov <fatah.guitar@gmail.com>
2022-04-05 13:06:02 +0700

    in Work on index.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,9 +1,8 @@
-import readlineSync from 'readline-sync';
-import { nameUser } from '../cli.js';
+import engineGame, { getRandomNum } from '../index.js';
 
-const getRandomNum = (min, max) => Math.round(Math.random() * (max - min) + min);
+const gameRules = console.log('What number is missing in the progression?');
 
-const startRound = () => {
+const bodyGame = () => {
   const giveProgressivNum = getRandomNum(2, 6);
   const hiddenNum = getRandomNum(0, 9);
   let countRaund = 1;
@@ -14,26 +13,9 @@ const startRound = () => {
   }
   const rightAnswer = arr[hiddenNum].toString();
   arr[hiddenNum] = '..';
-  const str = arr.join(' ');
-  const usersAnswer = readlineSync.question(`Question: ${str} `);
-  if (rightAnswer === usersAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${usersAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
-  return false;
+  const gameQuestion = arr.join(' ');
+  return [gameQuestion, rightAnswer];
 };
+const progressionGame = () => engineGame(gameRules, bodyGame);
 
-function progressionGame() {
-  console.log('What number is missing in the progression?');
-  const count = 3;
-  for (let i = 0; i < count; i += 1) {
-    const result = startRound();
-    if (result !== true) {
-      console.log(`Let's try again, ${nameUser}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${nameUser}!`);
-}
 export default progressionGame;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+export const getRandomNum = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+function engineGame(bodyGame) {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+  console.log(bodyGame[2]);
+  const count = 3;
+
+  for (let i = 0; i < count; i += 1) {
+    const [gameQuestion, rightAnswer] = bodyGame();
+    const usersAnswer = readlineSync.question(`Question: ${gameQuestion} `);
+    console.log(`Your answer: ${usersAnswer}`);
+    if (usersAnswer === rightAnswer) {
+      console.log('Correct!');
+    }
+    if (usersAnswer !== rightAnswer) {
+      console.log(`${usersAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
+      console.log(`Let's try again, ${nameUser}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${nameUser}!`);
+}
+export default engineGame;
