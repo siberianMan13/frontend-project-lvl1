04aa4686f0fd34e11f5dd45c725665fa0d6b0932
Ilshat Fattakhov <fatah.guitar@gmail.com>
2022-04-05 17:54:22 +0700

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,3 @@
-import readlineSync from 'readline-sync';
 import engineGame, { getRandomNum } from '../index.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
